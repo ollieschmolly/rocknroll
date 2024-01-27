a171a122423cfbcf58ac25ceb41f77e38f0ef3e7
ollieschmolly <ollieschmolly@gmail.com>
2024-01-26 20:59:27 -0600

    Commit before globalgamejam work

---
+++ rocknrollproto/Assets/Scripts/Simple2DCharacterController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Simple2DCharacterController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 10f;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    public bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Check if the player is on the ground
+        isGrounded = Physics2D.OverlapCircle(transform.position, 1f, groundLayer);
+
+        // Player movement
+        float horizontalInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
+
+        // Jumping
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+}
